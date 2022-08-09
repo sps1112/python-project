368623d85e5bd95649213c9d8b70743648a6b3a2
Siddhartha Pratap Singh <sidps1112@gmail.com>
2022-08-09 20:16:44 +0530

    Add functions

--- src/app.py
+++ src/app.py
@@ -1,36 +1,59 @@
 from math import *
 
-p1 = (1, 2)
-p2 = (3, 4)
-print(p1)
-print(p2[0])
-print(p2[1])
-p3 = (0.4, 0.5, 0.1)
-print(p3)
-points = [p1, p2, p3]
-print(points)
 
-names = ["Vito", "Sonny", "Tom", "Michael"]
-print(names)
-print(len(names))
-print(names[0] + " " + names[1])
-print(names[2:])
-print(names[1:3])
-names[1] = "Fredo"
-print(names)
-names.append("Sonny")
-print(names)
-names.insert(2, "Tessio")
-print(names)
-names.remove("Fredo")
-names.append("Clemenza")
-print(names)
-names.sort()
-print(names)
-names.reverse()
-print(names)
+def showcase_tuple(a, b):
+    p1 = (a, b)
+    p2 = (3, 4)
+    print(p1)
+    print(p2[0])
+    print(p2[1])
+    p3 = (0.4, 0.5, 0.1)
+    print(p3)
+    points = [p1, p2, p3]
+    print(points)
 
-names2 = names.copy()
-names2.insert(0, "Fredo")
-print(names2)
-print(names)
+
+def showcase_list():
+    names = ["Vito", "Sonny", "Tom", "Michael"]
+    print(names)
+    print(len(names))
+    print(names[0] + " " + names[1])
+    print(names[2:])
+    print(names[1: 3])
+    names[1] = "Fredo"
+    print(names)
+    names.append("Sonny")
+    print(names)
+    names.insert(2, "Tessio")
+    print(names)
+    names.remove("Fredo")
+    names.append("Clemenza")
+    print(names)
+    names.sort()
+    print(names)
+    names.reverse()
+    print(names)
+
+    names2 = names.copy()
+    names2.insert(0, "Fredo")
+    print(names2)
+    print(names)
+
+
+def calculate_eq(a, b, c):
+    d = a + b * c
+    return d
+
+
+print("LIST: ")
+showcase_list()
+print("")
+
+print("TUPLE: ")
+a = int(input("Number1: "))
+b = int(input("Number2: "))
+showcase_tuple(a, b)
+print("")
+
+print("EQUATION: ")
+print(calculate_eq(1, 2, 3))
