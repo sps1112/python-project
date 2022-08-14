dc623b4bbed2d25912df4e6ef40e70336e1902e9
Siddhartha Pratap Singh <sidps1112@gmail.com>
2022-08-14 14:38:12 +0530

    Add for loop and 2d lists

--- includes/standard_lib.py
+++ includes/standard_lib.py
@@ -2,6 +2,8 @@ from math import *
 
 
 def showcase_tuple(a, b):  # Showcases example function for tuples
+    print("Tuple Showcase".upper())
+    print("--------")
     p1 = (a, b)
     p2 = (3, 4)
     print(p1)
@@ -11,9 +13,13 @@ def showcase_tuple(a, b):  # Showcases example function for tuples
     print(p3)
     points = [p1, p2, p3]
     print(points)
+    print("--------")
+    print("")
 
 
 def showcase_list():  # Showcases example function for lists
+    print("List Showcase".upper())
+    print("--------")
     names = ["Vito", "Sonny", "Tom", "Michael"]
     print(names)
     print(len(names))
@@ -38,12 +44,37 @@ def showcase_list():  # Showcases example function for lists
     names2.insert(0, "Fredo")
     print(names2)
     print(names)
+    print("--------")
+    print("")
 
 
 def showcase_dictionary():  # Showcases example function for dictionaries
+    print("Dictionary Showcase".upper())
+    print("--------")
     options = {
         "A": "New Game",
         "B": "Options",
         "C": "Quit"
     }
     print(options["B"])
+    print(options["C"])
+    print(options["A"])
+    print("--------")
+    print("")
+
+
+def showcase_2d_list():  # Showcases a 2D List via for loops
+    print("2D List Showcase".upper())
+    print("--------")
+    list = [
+        [2, 5, 6, 8],
+        [1, 7, 9],
+        [0, 3],
+        [4]
+    ]
+    for row in list:
+        for index in range(len(row)):
+            print(row[index], end=" ")
+        print("")
+    print("--------")
+    print("")
